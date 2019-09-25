b870708261f432dd2e39372cf490269f304a7e05
NijeboerFrank <frank.nijeboer@gmail.com>
2019-09-25 18:39:09 +0200

    more plots

--- read_csv.py
+++ read_csv.py
@@ -3,3 +3,10 @@ import pandas as pd
 file = "Assignment 23-09-2019 - Forensics - Sheet1.csv"
 
 df = pd.read_csv(file)
+df["Legal"] = df["Legal"].apply(str.capitalize)
+df.groupby('Category')
+grouped_cats = df.groupby(['Category', 'Legal']).size().unstack()
+for grou in grouped_cats.iteritems():
+    print(grou)
+print(grouped_cats)
+
