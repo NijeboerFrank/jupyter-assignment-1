fad22280c098673c13d88a00a959329443666ad7
NijeboerFrank <frank.nijeboer@extendas.com>
2019-09-26 14:54:43 +0200

    sellers csv is ingelezen

--- read_csv.py
+++ read_csv.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
-file = "Assignment 23-09-2019 - Forensics - Sheet1.csv"
+posts = "Assignment 23-09-2019 - Forensics - Sheet1.csv"
 
-df = pd.read_csv(file)
+df = pd.read_csv(posts)
 df["Legal"] = df["Legal"].apply(str.capitalize)
+
+sellers = "Assignment 23-09-2019 - Forensics - Sellers.csv"
+
+seller_df = pd.read_csv(sellers)
 
 
